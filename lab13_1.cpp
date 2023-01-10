df2ca05022fe5f9d5be6c5806d890c5347d6c772
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-01-10 15:23:36 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-
-int fibonacci(int x){
+long long int fibonacci(long long int x){
 
     if(x == 1){
    return 1;
@@ -23,4 +25,3 @@ int fibonacci(int x){
     return fibonacci(x-1) + fibonacci(x-2) ;
   }
     }
-
