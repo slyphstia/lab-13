2a6ec5fee3b2f18579012d424228b6c83e973cfd
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-01-10 15:09:49 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+
+int fibonacci(int x){
+
+    if(x == 1){
+   return 1;
+    }
+    else if (x == 0){
+    return 0;
+    }
+  else{
+    return fibonacci(x-1) + fibonacci(x-2) ;
+  }
+    }
+
